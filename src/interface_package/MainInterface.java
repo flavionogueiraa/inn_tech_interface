513554163b00f78c09159213c419312720a741d3
flaviotech <flavio.nogueira.profissional@gmail.com>
2021-10-28 17:03:15 -0300

    Troca de telas

--- src/interface_package/MainInterface.java
+++ src/interface_package/MainInterface.java
@@ -7,17 +7,43 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainInterface extends Application {
-	@Override
-	public void start(Stage arg0) throws Exception {
-		Parent StartInterface = FXMLLoader.load(getClass().getResource("/interface_package/FxmlDoccument.fxml"));
-		Scene cena = new Scene(StartInterface);
 
-		arg0.setTitle("Meu novo programa com interface");
-		arg0.setScene(cena);
-		arg0.show();
+	private static Stage stage;
+
+	private static Scene cenaTeste;
+	private static Scene cenaLogin;
+
+	@Override
+	public void start(Stage primeiroEstagio) throws Exception {
+		stage = primeiroEstagio;
+
+		Parent FXMLTeste = FXMLLoader.load(getClass().getResource("/interface_package/FxmlDoccument.fxml"));
+		cenaTeste = new Scene(FXMLTeste);
+
+		Parent FXMLLogin = FXMLLoader.load(getClass().getResource("/interface_package/Login.fxml"));
+		cenaLogin = new Scene(FXMLLogin);
+
+		primeiroEstagio.setTitle("Meu novo programa com interface");
+		primeiroEstagio.setScene(cenaTeste);
+		primeiroEstagio.show();
 	}
 
 	public static void main(String[] args) {
 		launch(args);
+	}
+
+	public static void trocaTela(String nomeTela) {
+		switch (nomeTela) {
+		case "teste":
+			stage.setScene(cenaTeste);
+			break;
+
+		case "login":
+			stage.setScene(cenaLogin);
+			break;
+
+		default:
+			break;
+		}
 	}
 }
