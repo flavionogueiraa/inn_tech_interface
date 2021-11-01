b90d29fb0349422660d545b995ed0319c1ae66c3
flaviotech <flavio.nogueira.profissional@gmail.com>
2021-10-31 21:41:06 -0300

    Listagem de usuários

--- src/controler/UsuarioListagemControler.java
+++ src/controler/UsuarioListagemControler.java
@@ -1,5 +1,44 @@
 package controler;
 
-public class UsuarioListagemControler extends MenuControler {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import usuario.Usuario;
+
+public class UsuarioListagemControler extends MenuControler implements Initializable {
+	@FXML
+	private TableView<Usuario> tabela_usuarios;
+
+	@FXML
+	private TableColumn<Usuario, String> nome_usuario;
+
+	@FXML
+	private TableColumn<Usuario, String> cpf_usuario;
+
+	@FXML
+	private TableColumn<Usuario, Boolean> proprietario_usuario;
+	
+	@FXML
+    private TableColumn<Usuario, String> acoes_usuario;
+
+	ObservableList<Usuario> lista = FXCollections.observableArrayList();
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		nome_usuario.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getNome()));
+		cpf_usuario.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getCPF()));
+		proprietario_usuario.setCellValueFactory(data -> new SimpleBooleanProperty(data.getValue().isProprietario()));
+		acoes_usuario.setCellValueFactory(data -> new SimpleStringProperty("Teste"));
+		
+
+		tabela_usuarios.getItems().addAll(Usuario.usuarios);
+	}
 }
