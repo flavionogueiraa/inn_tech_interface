66002b4b55db95be57bf10f21d1a74603eb2e6ce
flaviotech <flavio.nogueira.profissional@gmail.com>
2021-11-04 20:38:44 -0300

    Primeira etapa de validação do CPF no cadastro pelo front

--- src/controler/UsuarioControler.java
+++ src/controler/UsuarioControler.java
@@ -11,25 +11,27 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import usuario.Usuario;
+import validacao.ValidacaoCPFInterface;
 
 public class UsuarioControler extends MenuControler implements Initializable {
 	@FXML
-    private TextField campo_cpf;
+	private TextField campo_cpf;
 
-    @FXML
-    private TextField campo_nome;
+	@FXML
+	private TextField campo_nome;
 
-    @FXML
-    private CheckBox campo_proprietario;
+	@FXML
+	private CheckBox campo_proprietario;
 
-    @FXML
-    private TextField campo_senha;
+	@FXML
+	private TextField campo_senha;
 
-    @FXML
+	@FXML
 	private TableView<Usuario> tabela_usuarios;
 
 	@FXML
@@ -43,33 +45,49 @@ public class UsuarioControler extends MenuControler implements Initializable {
 
 	@FXML
 	private TableColumn<Usuario, Boolean> proprietario_usuario;
-	
+
+	@FXML
+	private Label label_erro;
+
 	ObservableList<Usuario> lista = FXCollections.observableArrayList();
 
-    @FXML
-    void cadastrarUsuario(ActionEvent event) {
-    	String nome = campo_nome.getText();
-		String CPF = campo_cpf.getText();
-		String senha = campo_senha.getText();
-		Boolean proprietario = campo_proprietario.isSelected();
+	private void limparCampos() {
+		campo_nome.clear();
+		campo_cpf.clear();
+		campo_senha.clear();
+		campo_proprietario.setSelected(false);
+	}
 
-		Usuario novo_usuario = Usuario.cadastraUsuarioInterface(nome, CPF, senha, proprietario);
-		tabela_usuarios.getItems().add(novo_usuario);
-    }
+	@FXML
+	void cadastrarUsuario(ActionEvent event) {
+		label_erro.setText("");
+		String CPF = ValidacaoCPFInterface.validacaoNormal(campo_cpf.getText());
+		if (CPF != null) {
+			String nome = campo_nome.getText();
+			String senha = campo_senha.getText();
+			Boolean proprietario = campo_proprietario.isSelected();
 
-    @FXML
-    void editarUsuario(ActionEvent event) {
-    	System.out.println(tabela_usuarios.getSelectionModel().getSelectedItem().getNome());
-    }
+			Usuario novo_usuario = Usuario.cadastraUsuarioInterface(nome, CPF, senha, proprietario);
+			tabela_usuarios.getItems().add(novo_usuario);
+			limparCampos();
+		} else {
+			label_erro.setText("CPF inválido");
+		}
+	}
 
-    @FXML
-    void excluirUsuario(ActionEvent event) {
-    	Usuario usuario = tabela_usuarios.getSelectionModel().getSelectedItem();
-    	tabela_usuarios.getItems().remove(usuario);
-    	usuario.deletaUsuario();
-    }
-    
-    @Override
+	@FXML
+	void editarUsuario(ActionEvent event) {
+		System.out.println(tabela_usuarios.getSelectionModel().getSelectedItem().getNome());
+	}
+
+	@FXML
+	void excluirUsuario(ActionEvent event) {
+		Usuario usuario = tabela_usuarios.getSelectionModel().getSelectedItem();
+		tabela_usuarios.getItems().remove(usuario);
+		usuario.deletaUsuario();
+	}
+
+	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		nome_usuario.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getNome()));
 		cpf_usuario.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getCPF()));
